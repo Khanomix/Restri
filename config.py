d0904ae45a9589b36623c1efbd012701cb506136
Khanomix <123134542+Khanomix@users.noreply.github.com>
2024-09-15 20:09:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "18991521"))
+API_HASH = getenv("API_HASH", "0c1086e0013e5f6633ad3b4bae13238b")
+BOT_TOKEN = getenv("BOT_TOKEN", "7516196344:AAF5FVbcSLK7Zat-JjR6TO8SAP3fdoqlsWY")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1633416742").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://hehup:hehup18@cluster0.ema1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002139381519")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002145995438"))
